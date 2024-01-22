fa409ba564a925bca1d2ff0fb845e7f2d8b1bc84
asitouze <aurelien.sitouze@gmail.com>
2024-01-22 11:48:10 +0100

    put is end

--- src/app.js
+++ src/app.js
@@ -10,7 +10,10 @@ app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
 app.get("/api/users/:id", movieControllers.getUsersById);
 app.post("/api/movies", movieControllers.postMovie);
-app.post("/api/users", movieControllers.postUsers)
+app.post("/api/users", movieControllers.postUsers);
+app.put("/api/movies/:id", movieControllers.updateMovie);
+app.put("/api/users/:id", movieControllers.updateUsers);
+
 
 
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -124,6 +124,50 @@ const postUsers = (req, res) => {
     });
 }
 
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const updateUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "UPDATE movies SET title = ?, director = ?, year = ?, color = ?, duration = ? WHERE id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+}
+
 module.exports = {
   getMovies,
   getMovieById,
@@ -131,4 +175,6 @@ module.exports = {
   postUsers,
   getUsers,
   getUsersById,
+  updateMovie,
+  updateUsers,
 };
