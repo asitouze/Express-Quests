3642bb8e97d9f7779d95cdba5335616feac83880
asitouze <aurelien.sitouze@gmail.com>
2024-02-12 09:34:53 +0100

    save

--- router/router.js
+++ router/router.js
@@ -3,8 +3,8 @@ const userControllers = require("../src/controllers/userControllers");
 const { hashPassword } = require("../auth");
 const router = express.Router();
 
-// router.get("/api/movies", movieControllers.getMovies);
-// router.get("/api/movies/:id", movieControllers.getMovieById);
+router.get("/api/movies", movieControllers.getMovies);
+router.get("/api/movies/:id", movieControllers.getMovieById);
 router.get("/api/users", userControllers.getAllUsers);
 router.post("/api/users", hashPassword, userControllers.postUser);
 router.put("/api/users/:id", hashPassword, userControllers.putUser);
--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ const router = require("../router/router");
 // const movieControllers = require("./controllers/movieControllers");
 // const userHandlers = require("./controllers/userControllers");
 app.use(express.json());
+const verifyToken = require("../middleware/verifyToken");
+app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.use(router);
 module.exports = app;
